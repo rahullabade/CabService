0f59a1df60e9b5a7855be12394dd02ab6e3d8a1e
RAHUL LABADE <rahullabade05@gmail.com>
2021-11-12 11:58:42 +0530

    Added: functioning for multiple fare

--- java/com/company/InvoiceGenerator.java
+++ java/com/company/InvoiceGenerator.java
@@ -2,9 +2,21 @@ package com.company;
 
 public class InvoiceGenerator {
     private static final int COST_PER_TIME = 1;
-    private static final double THE_DISTANCE_OF_EACH_KM = 10;
+    private static final double MINIMUM_COST_PER_KILOMETER = 10;
+    private static final int MINIMUM_FARE = 5;
 
-    public double calculateFare(double distance , int time ) {
-        return  distance * THE_DISTANCE_OF_EACH_KM + time * COST_PER_TIME;
+    public double CalculateFare(double distance, int time) {
+        double totalDistance = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+        if (totalDistance < MINIMUM_FARE)
+            return MINIMUM_FARE;
+        return totalDistance;
+    }
+
+    public double CalculateFare(Ride[] rides) {
+        double totalFare = 0;
+        for (Ride ride : rides) {
+            totalFare += this.CalculateFare(ride.distance, ride.time);
+        }
+        return totalFare;
     }
 }
